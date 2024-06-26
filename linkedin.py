0b1893c8fa9ec360fe387e5b3298c3356da49122
Paras Savnani <savnani5@gmail.com>
2024-06-26 16:34:10 -0700

    cleanup

---
+++ linkedin.py
@@ -0,0 +1,25 @@
+# import requests
+
+# api_key = 'gx6PW2_VrTEC_WTdXBoicw'
+# headers = {'Authorization': 'Bearer ' + api_key}
+# api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin'
+# params = {
+#     'twitter_profile_url': '',
+#     'facebook_profile_url': '',
+#     'linkedin_profile_url': 'https://linkedin.com/in/paras-savnani-532966182/',
+#     'extra': 'include',
+#     'github_profile_id': 'include',
+#     'facebook_profile_id': 'include',
+#     'twitter_profile_id': 'include',
+#     'personal_contact_number': 'include',
+#     'personal_email': 'include',
+#     'inferred_salary': 'include',
+#     'skills': 'include',
+#     'use_cache': 'if-present',
+#     'fallback_to_cache': 'on-error',
+# }
+# response = requests.get(api_endpoint,
+#                         params=params,
+#                         headers=headers)
+# print(response.json())
+
--- slack/functions.py
+++ slack/functions.py
@@ -10,7 +10,7 @@ from langchain.prompts.chat import (
 load_dotenv(find_dotenv())
 
 
-def draft_email(user_input, name="Dave"):
+def draft_email(user_input, name="Paras"):
     chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=1)
 
     template = """
